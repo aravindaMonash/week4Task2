4d76cf99232ec202d0c2306faef0f9ebfbf1a88d
aban0036 <aban0036@MV2NDCSTUL05003>
2023-08-20 22:44:50 +1000

    Add project files.

---
+++ CodeFirst/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Code_First_Approach_in_Entity_Framework.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+    }
+}
---
+++ CodeFirst/Models/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+namespace Code_First_Approach_in_Entity_Framework.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
